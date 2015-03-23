2ca479f5c4701356a153a691a6bc897aa26839b1
ashwintumma23 <ashwin.tumma23@gmail.com>
2015-03-23 01:44:49 -0400

    Updated the descriptions for Format and Calendar

--- src/CalendarHeatMap/GetDateValues.py
+++ src/CalendarHeatMap/GetDateValues.py
@@ -1,16 +1,40 @@
+#############################################################################################
+
+# Python script for extracting the values of the start date of the events of Eventbrite
+
+# Author: Ashwin Tumma <ashwin.tumma@stonybrook.edu>
+# Date: March 20, 2015
+
+# Central Idea: 
+# -------------
+# We have already obtained the data for the events' start date using a separate python file, and 
+# have the data available in LocationDateFormatData.txt file. We will be using the start date values 
+# from this file. 
+
+# Usage: 
+# ----------
+# $ python GetDateValues.py 
+# The output of this program will be a file named - dateTimeJSON.json which will contain 
+# all the data points encoded in the epoch time value format 
+
+#############################################################################################
+
 import time
 
 epochTimeDictionary = {}
 
 pattern = '%Y-%m-%d %H:%M:%S'
-
 pointsFile = open('LocationDateFormatData.txt')
 
-
+# Extract the values of the start date from the input file
 for line in pointsFile:
 	try:
 		dateTimeValue = line.split(' ,')[2]
 		if dateTimeValue != "None".strip():
+
+			# Convert the given date time value from its format to UNIX epoch time
+			# We convert the timestamp value to epoch time, because cal-heatmap.js expects data 
+			# in the UNIX epoch time value format.
 			dateTimeValue = dateTimeValue.replace("T"," ")
 			print dateTimeValue
 			epoch = int(time.mktime(time.strptime(dateTimeValue.strip(), pattern)))
@@ -19,12 +43,13 @@ for line in pointsFile:
 				epochTimeDictionary[epoch] = epochTimeDictionary.get(epoch) + 1
 			else: 
 				epochTimeDictionary[epoch] = 1
-			print "EPOCH = ", epochTimeDictionary[epoch]
 	except:
 		continue
 
 
+pointsFile.close()
 
+# Start writing data to the output JSON file
 jsonOutputFile = open('dateTimeJSON.json','wb')
 jsonOutputFile.write("{") 
 
@@ -33,8 +58,4 @@ for epochTime, value in epochTimeDictionary.items():
 
 jsonOutputFile.write('"0":0 }')
 jsonOutputFile.close()
-pointsFile.close()
-
-
-
 
--- src/FormatDataRepresentation/GetFormatStatistics.py
+++ src/FormatDataRepresentation/GetFormatStatistics.py
@@ -1,13 +1,35 @@
-# Start writing the data points value to the output file
+#############################################################################################
 
+# Python script for the following tasks: 
+# 1. Get the different format types from the Eventbrite JSON API
+# 2. Create a dictionary to store the values of the formatIDs for all the events on Eventbrite
+# 3. Generate a JSON file which will be the input to the bubble chart D3 rendering 
 
-        # The token mentioned in the URL is the token which I got generated from the Event Brite API User Interface
-        # For security purposes I have masked the token with all Xs in the published version of this Python code, 
-        # but you can get yours generated similary from Event Brite's Generate Token Page.
+# Author: Ashwin Tumma <ashwin.tumma@stonybrook.edu>
+# Date: March 20, 2015
+
+# Central Idea: 
+# -------------
+# We have already obtained the data for the events' formatID using a separate python file, and 
+# have the data available in LocationDateFormatData.txt file. We will be using the formatID value
+# from this file. 
+
+# Usage: 
+# ----------
+# $ python GetFormatStatistics.py 
+# The output of this program will be a file named - format.json which will contain 
+# all the data points encoded in the formatID value
+
+#############################################################################################
+
 import urllib2
 import json
+
+# The token mentioned in the URL is the token which I got generated from the Event Brite API User Interface
+# For security purposes I have masked the token with all Xs in the published version of this Python code, 
+# but you can get yours generated similary from Event Brite's Generate Token Page.
  
-response = urllib2.urlopen('https://www.eventbriteapi.com/v3/formats/?token=CVFVRR3UQX4IGUCYTFBK')
+response = urllib2.urlopen('https://www.eventbriteapi.com/v3/formats/?token=XXXXXXXXXXXXXXX')
 data = json.load(response)
 
 formats = data['formats']
@@ -17,7 +39,7 @@ formatDict = {}
 formatNamesDict = {}
 formatLongNamesDict = {}
 
-print "Printing the ids now"
+# Get the values from the JSON API
 for k in formats:
 	# Initialize the dictionary with the format ids as the key and ZERO as its value
 	formatDict[k['id']] = 0;
@@ -27,7 +49,6 @@ for k in formats:
 	
 
 # Populate the dictionary from the values that we have fetched earlier
-
 pointsFile = open('LocationDateFormatData.txt')
 for line in pointsFile:
 	formatID =  line.split(' ,')[3].strip()
